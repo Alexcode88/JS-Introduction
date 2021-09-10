fb6da8883541f4d7430a14aa7787d8bf64079a30
Alfredo08 <alfredosv08@gmail.com>
2021-09-10 16:30:50 -0500

    Completing the fizzbuzz challenge

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,3 +9,28 @@
     1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
 
 */
+
+function fizzBuzzSequence( limit ){
+    for( let i = 1; i <= limit; i ++ ){
+        if( i % 5 === 0 && i % 3 === 0 ){
+            console.log( "FizzBuzz" );
+        }
+        else{
+            if( i % 5 === 0 ){
+                console.log( "Buzz" );
+            }
+            else{
+                if( i % 3 === 0 ){
+                    console.log( "Fizz" );
+                }
+                else{
+                    console.log( i );
+                }
+            }
+        }
+    }
+}
+
+fizzBuzzSequence( 20 );
+console.log( "------------" );
+fizzBuzzSequence( 100 );
